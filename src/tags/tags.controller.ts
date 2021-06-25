7c313824f011cb6a28d676ad6235ab9ede494431
Alexis navarro <alexthebigboss1@gmail.com>
2021-06-25 15:45:19 -0400

    fix(tags): fix tags controller guards

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query, UseGuards, UseInterceptors } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { Roles } from 'src/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { ParamsToBodyInterceptor } from 'src/support/interceptors/params-to-body.interceptor';
 import { PaginationResult } from 'src/support/pagination/pagination-result';
@@ -16,29 +17,25 @@ export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
   @Get()
-  @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
   async paginate(@Query(TagPaginationPipe) options: any): Promise<PaginationResult<ReadTagDto>> {
     return (await this.tagsService.paginate(options)).toClass(ReadTagDto);
   }
 
   @Post()
   @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   async create(@Body() createTagDto: CreateTagDto): Promise<ReadTagDto> {
     return plainToClass(ReadTagDto, await this.tagsService.create(createTagDto));
   }
 
   @Get(':id')
-  @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
   async findOne(@Param('id') id: string): Promise<ReadTagDto> {
     return plainToClass(ReadTagDto, await this.tagsService.findOne(+id));
   }
 
   @Put(':id')
   @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @UseInterceptors(new ParamsToBodyInterceptor({id: 'id'}))
   async update(@Body() updateTagDto: UpdateTagDto): Promise<ReadTagDto> {
     return plainToClass(ReadTagDto, await this.tagsService.udpate(updateTagDto));
@@ -46,7 +43,7 @@ export class TagsController {
 
   @Delete(':id')
   @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param('id') id: string): Promise<void> {
     return await this.tagsService.delete(+id);
