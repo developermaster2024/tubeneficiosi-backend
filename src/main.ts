3c2cc9dcaa4c48a7b2a7ce8d60cd30eb8b3d1c60
Alexis navarro <alexthebigboss1@gmail.com>
2021-06-29 11:10:38 -0400

    feat(app): add configuration to serve static files(images)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { useContainer } from 'class-validator';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   app.enableCors();
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {prefix: '/uploads'});
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
