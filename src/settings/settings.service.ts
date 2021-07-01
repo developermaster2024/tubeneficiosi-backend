daa695baa5f4b9469c1808f1ace3fb0c79e6c91d
Alexis navarro <alexthebigboss1@gmail.com>
2021-07-01 17:25:45 -0400

    feat(settings): add id to footer sections

--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -132,6 +132,7 @@ export class SettingsService {
     setting.value = {
       ...setting.value,
       [sectionName]: {
+        id,
         ...updateFooterSectionDto,
         widgets: mappedWidgets,
       },
