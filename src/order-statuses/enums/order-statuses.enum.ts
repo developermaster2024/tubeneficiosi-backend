79488de6832f3779b51e97eceb4febf9e5fd2fbe
Alexis navarro <alexthebigboss1@gmail.com>
2021-08-25 15:31:15 -0400

    feat(order statuses): add missing status

--- src/order-statuses/enums/order-statuses.enum.ts
+++ src/order-statuses/enums/order-statuses.enum.ts
@@ -1,3 +1,8 @@
 export enum OrderStatuses {
   CONFIRMING_PAYMENT = 'ors-001',
+  PAYMENT_ACCEPTED = 'ors-002',
+  PAYMENT_REJECTED = 'ors-003',
+  SENDING_PRODUCTS = 'ors-004',
+  PRODUCTS_SENT = 'ors-005',
+  SHIPPING_ERROR = 'ors-006',
 }
