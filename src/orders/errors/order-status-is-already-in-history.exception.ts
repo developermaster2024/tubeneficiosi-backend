52d2e73fc65bfeea0d9ebea8373542ec9c1facfd
Alexis navarro <alexthebigboss1@gmail.com>
2021-08-26 17:26:11 -0400

    refactor(orders): change conflict status code with 422

--- src/orders/errors/order-status-is-already-in-history.exception.ts
+++ src/orders/errors/order-status-is-already-in-history.exception.ts
@@ -4,6 +4,6 @@ export class OrderStatusIsAlreadyInHistoryException extends HttpException {
   constructor() {
     super({
       message: 'Estatus de orden ya existe en al historia',
-    }, HttpStatus.CONFLICT);
+    }, HttpStatus.UNPROCESSABLE_ENTITY);
   }
 }
