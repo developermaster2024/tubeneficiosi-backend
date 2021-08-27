80bc6f2e2ae3c4578a8dd3a8ade515b6b46b1322
Alexis navarro <alexthebigboss1@gmail.com>
2021-08-27 17:51:16 -0400

    feat(order statuses): add missing filters

--- src/order-statuses/order-statuses.controller.ts
+++ src/order-statuses/order-statuses.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { PaginationPipe } from 'src/support/pagination/pagination-pipe';
 import { PaginationResult } from 'src/support/pagination/pagination-result';
 import { OrderStatus } from './entities/order-status.entity';
 import { OrderStatusesService } from './order-statuses.service';
+import { OrderStatusPaginationPipe } from './pipes/order-status-pagination.pipe';
 
 @Controller('order-statuses')
 export class OrderStatusesController {
   constructor(private readonly orderStatusesService: OrderStatusesService) {}
 
   @Get()
-  async paginate(@Query(PaginationPipe) options: any): Promise<PaginationResult<OrderStatus>> {
+  async paginate(@Query(OrderStatusPaginationPipe) options: any): Promise<PaginationResult<OrderStatus>> {
     return await this.orderStatusesService.paginate(options);
   }
 }
--- src/order-statuses/order-statuses.service.ts
+++ src/order-statuses/order-statuses.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { PaginationOptions } from 'src/support/pagination/pagination-options';
 import { PaginationResult } from 'src/support/pagination/pagination-result';
 import { Repository } from 'typeorm';
+import { OrderStatusPaginationOptionsDto } from './dto/order-status-pagination-options.dto';
 import { OrderStatus } from './entities/order-status.entity';
 
 @Injectable()
 export class OrderStatusesService {
   constructor(@InjectRepository(OrderStatus) private readonly orderStatusesRepository: Repository<OrderStatus>) {}
 
-  async paginate({perPage, offset}: PaginationOptions): Promise<PaginationResult<OrderStatus>> {
-    const [orderStatuses, total] = await this.orderStatusesRepository.createQueryBuilder('orderStatus')
+  async paginate({perPage, offset, filters: {
+    code,
+    allowedByCodeAndRole,
+  }}: OrderStatusPaginationOptionsDto): Promise<PaginationResult<OrderStatus>> {
+    const queryBuilder = this.orderStatusesRepository.createQueryBuilder('orderStatus')
       .take(perPage)
-      .skip(offset)
-      .leftJoinAndSelect('orderStatus.allowedOrderStatuses', 'allowedOrderStatuses')
-      .leftJoinAndSelect('allowedOrderStatuses.allowedOrderStatus', 'allowedOrderStatus')
-      .getManyAndCount();
+      .skip(offset);
+
+    if (code) queryBuilder.andWhere('orderStatus.code = :code', { code });
+
+    if (allowedByCodeAndRole?.length > 0) {
+      const [allowedByCode, role] = allowedByCodeAndRole;
+
+      queryBuilder.andWhere(`orderStatus.code IN (
+        SELECT
+          ostos.allowed_order_status_code
+        FROM
+          order_status_to_order_status ostos
+        WHERE
+          ostos.order_status_code = :allowedByCode AND
+          ostos.role = :role
+      )`, { allowedByCode, role });
+    }
+
+    const [orderStatuses, total] = await queryBuilder.getManyAndCount();
 
     return new PaginationResult(orderStatuses, total, perPage);
   }
