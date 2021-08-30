3a82bc72f347ee1a93db7165b03f719d3c152972
Alexis navarro <alexthebigboss1@gmail.com>
2021-08-30 15:25:39 -0400

    reafactor(notifications): remove unnecessary field

--- src/notifications/entities/notification.entity.ts
+++ src/notifications/entities/notification.entity.ts
@@ -26,13 +26,6 @@ export class Notification {
   date: Date;
 
   @Column({
-    name: 'read_on',
-    type: 'datetime',
-    nullable: true,
-  })
-  readOn: Date;
-
-  @Column({
     name: 'role',
     type: 'varchar',
     length: 50,
