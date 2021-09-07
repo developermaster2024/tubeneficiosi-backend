7dcee10b4a2e3dc6aba4b493bb92b95de25ebdf6
Alexis navarro <alexthebigboss1@gmail.com>
2021-09-07 15:18:26 -0400

    feat(websockets): log number of connections

--- src/notifications/notifications.gateway.ts
+++ src/notifications/notifications.gateway.ts
@@ -1,9 +1,17 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { Role } from 'src/users/enums/roles.enum';
 
 @WebSocketGateway()
-export class NotificationsGateway {
+export class NotificationsGateway implements OnGatewayConnection {
+  static clientsCount = 0;
+
+  handleConnection(client: any, ...args: any[]) {
+    NotificationsGateway.clientsCount++;
+
+    console.log({clientsCount: NotificationsGateway.clientsCount});
+  }
+
   @WebSocketServer()
   public server: Server;
 
