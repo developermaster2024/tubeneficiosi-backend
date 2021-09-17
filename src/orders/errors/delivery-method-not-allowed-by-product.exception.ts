428db2af58407c2a88a46f48f7b521cf12033ebe
Alexis navarro <alexthebigboss1@gmail.com>
2021-09-17 16:47:42 -0400

    feat(delivery methods): add code to exception

--- src/orders/errors/delivery-method-not-allowed-by-product.exception.ts
+++ src/orders/errors/delivery-method-not-allowed-by-product.exception.ts
@@ -5,6 +5,7 @@ export class DeliveryMethodNotAllowedByProductException extends HttpException {
   constructor(product: Product) {
     super({
       message: 'Método de envío no permitido por el producto',
+      code: 'dmt-001',
       product,
     }, HttpStatus.CONFLICT);
   }
