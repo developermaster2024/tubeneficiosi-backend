10c8345656061b54272490240145b1eea8ca641f
Alexis navarro <alexthebigboss1@gmail.com>
2021-11-04 14:18:03 -0400

    feat(carts): fix product validation when adding to cart

--- src/carts/dto/add-to-cart.dto.ts
+++ src/carts/dto/add-to-cart.dto.ts
@@ -1,6 +1,7 @@
 import { Exclude, Expose, Type } from "class-transformer";
 import { IsBoolean, IsNumber, Min, ValidateNested } from "class-validator";
 import { Product } from "src/products/entities/product.entity";
+import { StoreCategoriesWithSchedules } from "src/store-categories/enum/store-category.enum";
 import { Store } from "src/stores/entities/store.entity";
 import { Exists } from "src/validation/exists.constrain";
 import { ProductFeaturesDto } from "./product-features.dto";
@@ -21,8 +22,19 @@ export class AddToCartDto {
   readonly quantity: number;
 
   @Expose()
-  @Exists(Product, 'id', (value, dto: AddToCartDto) => ({
-    where: {id: value, storeId: dto.storeId}
+  @Exists(Product, 'id', (id, { storeId }: AddToCartDto) => ({
+    join: {
+      alias: 'product',
+      innerJoin: {
+        store: 'product.store',
+        storeCategory: 'store.storeCategory',
+      },
+    },
+    where: qb => {
+      qb.where({ id })
+        .andWhere('store.id = :storeId', { storeId })
+        .andWhere('storeCategory.name NOT IN(:...storeCategoryNames)', { storeCategoryNames: StoreCategoriesWithSchedules });
+    }
   }))
   readonly productId: number;
 
