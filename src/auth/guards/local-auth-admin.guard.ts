6a757e11ed02056a77fdb4df569fad451729721a
Alexis Navarro <alexisnavarro1994@gmail.com>
2022-05-05 10:21:22 -0400

    feat(admin auth): show different message when no credentials are provided

--- src/auth/guards/local-auth-admin.guard.ts
+++ src/auth/guards/local-auth-admin.guard.ts
@@ -1,5 +1,13 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {AuthGuard} from "@nestjs/passport";
 
 @Injectable()
-export class LocalAuthAdminGuard extends AuthGuard('local-admin') {}
+export class LocalAuthAdminGuard extends AuthGuard('local-admin') {
+    handleRequest<TUser = any>(err: any, user: any, info: any, context: any, status?: any): TUser {
+        if (user === false) {
+            throw new UnauthorizedException('Debe ingresar sus credenciales');
+        }
+
+        return super.handleRequest(err, user, info, context, status);
+    }
+}
